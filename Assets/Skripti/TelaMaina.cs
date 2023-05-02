68d51c3c246c0efcfbc17e656008f3ec1c1d157f
Bumerts <bumertstomijs@gmail.com>
2023-05-02 10:51:58 +0300

    Projekta dzimuma mainas pievienosana

---
+++ Assets/Skripti/TelaMaina.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TelaMaina : MonoBehaviour {
+    public Sprite[] atteluMasivs;
+    public GameObject mainigaisAttels;
+    public void DzimumuMaina(int skaitlis) {
+        if (skaitlis == 0)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        }
+        else if (skaitlis == 1)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        }
+        else {
+            Debug.Log("Nav piesaistīts attēls!");
+        }
+    }
+}
