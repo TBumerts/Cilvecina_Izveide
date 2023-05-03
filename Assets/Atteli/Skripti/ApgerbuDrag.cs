20be9a34779727ebde1ea467812b9afcc1f14939
Bumerts <bumertstomijs@gmail.com>
2023-05-03 11:15:40 +0300

    Dazu attelu pievienosana ar iespeju drag&drop

---
+++ Assets/Atteli/Skripti/ApgerbuDrag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ApgerbuDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public RectTransform transformKomponente;
+    public Canvas kanva;
+    void start() {
+        transformKomponente = GetComponent<RectTransform>();
+    }
+    public void OnPointerDown(PointerEventData notikums)
+    {
+        Debug.Log("Izdarīts peles klikškis uz objekta!");
+
+    }
+    public void OnBeginDrag(PointerEventData notikums)
+    {
+        Debug.Log("Uzsākta objekta vilkšana!");
+    }
+    public void OnDrag(PointerEventData notikums)
+    {
+        Debug.Log("Notiek objekta pārvietošana!");
+        transformKomponente.anchoredPosition += notikums.delta / kanva.scaleFactor;
+    }
+    public void OnEndDrag(PointerEventData notikums)
+    {
+        Debug.Log("Objekta pārvietošana beigta!");
+    }
+}
---
+++ Assets/Atteli/Skripti/ObjektuPaslepsana.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjektuPaslepsana : MonoBehaviour {
+    public GameObject brunasimg;
+    public GameObject cepures;
+    public void brunuAttelosana(bool vertiva) {
+        brunasimg.SetActive(vertiva);
+    }
+}
