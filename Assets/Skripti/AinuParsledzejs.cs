3b72a9e064ccffe4fc2a337538e7266acc07f450
TBumerts <bumertstomijs@gmail.com>
2023-05-04 16:21:24 +0300

    Sakuma ekarana izveide un audio pievienosana

---
+++ Assets/Skripti/AinuParsledzejs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AinuParsledzejs : MonoBehaviour {
+
+	// Use this for initialization
+	public void uzMain () {
+		SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+	}
+	
+	// Update is called once per frame
+	public void uzPargerbsanu () {
+		SceneManager.LoadScene("Ainas", LoadSceneMode.Single);
+	}
+	public void Apturet() { 
+		Application.Quit();
+	}
+}
---
+++ Assets/Skripti/ObjektuPaslepsana.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjektuPaslepsana : MonoBehaviour {
+    public GameObject bronzasimg;
+	public GameObject dzelzsimg;
+	public GameObject melnasimg;
+	public GameObject sakausejumaimg;
+	public GameObject sefaimg;
+	public GameObject kronisimg;
+	public GameObject burvisimg;
+	public GameObject santaimg;
+	public GameObject gnomeimg;
+	public GameObject zilaimg;
+	public GameObject bronzasbimg;
+	public GameObject melnasbimg;
+	public GameObject dzelzsbimg;
+	public GameObject sakausejumabimg;
+    public void brunuAttelosana(bool vertiba) {
+        bronzasimg.SetActive(vertiba);
+		dzelzsimg.SetActive(vertiba);
+		melnasimg.SetActive(vertiba);
+		sakausejumaimg.SetActive(vertiba);
+
+    }
+	public void cepuruAttelosana(bool vertiba){
+		sefaimg.SetActive(vertiba);
+		kronisimg.SetActive(vertiba);
+		burvisimg.SetActive(vertiba);
+		santaimg.SetActive(vertiba);
+		gnomeimg.SetActive(vertiba);
+		zilaimg.SetActive (vertiba);
+	}
+	public void biksesAttelosana(bool vertiba) {
+		bronzasbimg.SetActive(vertiba);
+		melnasbimg.SetActive(vertiba);
+		dzelzsbimg.SetActive(vertiba);
+		sakausejumabimg.SetActive(vertiba);	
+	}
+}
